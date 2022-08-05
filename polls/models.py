69a10e13b71eaf07d4037bb96b02cf1d5655a6a1
Mfirs <mfirstborn@gmail.comgit config --global user.name Mfirs  git config --global user.email mfirstborn@gmail.com>
2022-08-05 10:53:33 +0200

    polls: Updated Polls model and make migration

--- polls/models.py
+++ polls/models.py
@@ -5,11 +5,19 @@ from django.db import models
 class Question(models.Model):
     # id = models.PrimaryKey(auto_increment=True)
     question_text = models.CharField(max_length=255)
+    question_description = models.CharField(max_length=255, default="Basic Description")
     pub_date = models.DateTimeField(auto_now_add=True)
 
-
+    def __str__(self):
+        return self.question_text
+    
 # 1)  "Are you happy about Django"         "June 16, 09:50 WAT"
 # 2)  "Are you ..."                        "June 17, 10:00 WAT"
+
+# Create Table question(
+#     question_text str;
+#     pub_date date;
+# )
 
 
 class Choices(models.Model):
@@ -21,6 +29,9 @@ class Choices(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_answer = models.CharField(max_length=20)
     vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_answer
 
 
     # 1)  "Are you happy about Django"        yes/no     3/0
